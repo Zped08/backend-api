d32dc48fd6c41dd24a663eba09c2b05c5ecc74b5
Jair Flores <jairflores0802@gmail.com>
2023-03-14 18:36:14 +0100

    ya

--- mongo.js
+++ mongo.js
@@ -2,19 +2,22 @@ const mongoose = require("mongoose");
 //const password = require("./password.js");
 //const { Schema, model } = mongoose;
 
+mongoose.set('strictQuery', false)
+
 const connectionString = process.env.MONGO_DB_URI
 
+console.log('connecting to', connectionString)
 // conexion a mongodb
 
-mongoose.connect(connectionString, {
+mongoose.connect(connectionString)
     /*     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true */
-  })
+  
   .then(() => {
-    console.log("database connected");
+    console.log("connected to MongoDB");
   })
-  .catch((err) => {
-    console.error(err);
+  .catch((error) => {
+    console.log('error connecting to mongodb:', error.message);
   });
